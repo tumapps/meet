cf4ad0d7f0ded624ffb139e23f17c50fe9f40599
Francis Yuppie <dcs00922022@students.tum.ac.ke>
2024-09-15 22:40:21 +0300

    fixed refreshtoken endpoint

--- modules/auth/controllers/AuthController.php
+++ modules/auth/controllers/AuthController.php
@@ -10,6 +10,7 @@ use auth\models\static\PasswordReset;
 use auth\models\static\ChangePassword;
 use auth\models\static\PasswordResetRequest;
 use auth\models\RefreshToken;
+use auth\models\User;
 use auth\models\searches\UserSearch;
 use auth\models\Profiles;
 use yii\base\InvalidArgumentException;
@@ -59,8 +60,7 @@ class AuthController extends \helpers\ApiController
 		if ($model->load($dataRequest) && $model->login()) {
 			$user = Yii::$app->user->identity;
 			$this->generateRefreshToken($user);
-			$accesToken = $this->getToken();
-			return $this->payloadResponse(['username' => $user->username, 'token' => $accesToken, 'menus' => Yii::$app->params['menus']], ['statusCode' => 200, 'message' => 'Access granted']);
+			return $this->payloadResponse(['username' => $user->username, 'token' => $user->token, 'menus' => Yii::$app->params['menus']], ['statusCode' => 200, 'message' => 'Access granted']);
 		}
 		return $this->errorResponse($model->getErrors());
 	}
@@ -159,59 +159,22 @@ class AuthController extends \helpers\ApiController
 		return $this->payloadResponse($user->profile, ['statusCode' => 201]);
 	}
 
-	public function getToken(): string
-	{
-		/** @var Jwt $jwt */
-		$jwt = Yii::$app->jwt;
-		if (!$jwt->key) {
-			throw new \yii\web\ServerErrorHttpException(Yii::t('app', 'The JWT secret must be configured first.'));
-		}
-
-		$signer = new \Lcobucci\JWT\Signer\Hmac\Sha256();
-		$time = time();
-		$token = $jwt->getBuilder()
-			->setIssuer(Yii::$app->params['jwt.issuer'])
-			->setAudience(Yii::$app->params['jwt.audience'])
-			->setId(Yii::$app->params['jwt.id'], true)
-			->setIssuedAt(time())
-			->setExpiration($time + 60)
-			->set('user_id', Yii::$app->user->identity->id)
-			->set('username', Yii::$app->user->identity->username)
-			->set('menus', Yii::$app->params['menus'])
-			->sign($signer, $jwt->key)
-			->getToken();
-
-		return (string)$token;
-	}
-
 	public function actionRefresh()
 	{
 		$refreshToken = Yii::$app->request->cookies->getValue('refresh-token', false);
 		if (!$refreshToken) {
-			return $this->errorResponse([
-				'statusCode' => [
-					'440'
-				]
-			]);
+			return $this->errorResponse(440);
 		}
 		$userRefreshToken = RefreshToken::findOne(['token' => $refreshToken]);
 		if (Yii::$app->request->getMethod() == 'POST') {
 			// Getting new JWT after it has expired
 			if (empty($userRefreshToken)) {
-				return $this->errorResponse([
-					'statusCode' => [
-						'440'
-					]
-				]);
+				return $this->errorResponse(440);
 			}
-			$user = \auth\models\User::findIdentity($userRefreshToken->user_id);
+			$user = User::findIdentity($userRefreshToken->user_id);
 			if (empty($user)) {
 				$userRefreshToken->delete();
-				return $this->errorResponse([
-					'statusCode' => [
-						'440'
-					]
-				]);
+				return $this->errorResponse(440);
 			}
 			$this->generateRefreshToken($user);
 			return $this->payloadResponse(['username' => $user->username, 'token' => (string) $user->token], ['statusCode' => 200]);
@@ -222,15 +185,11 @@ class AuthController extends \helpers\ApiController
 			}
 			return $this->toastResponse(['statusCode' => 200, 'message' => 'Logout successful']);
 		}
-		return $this->errorResponse([
-			'statusCode' => [
-				'500'
-			]
-		]);
+		return $this->errorResponse(500);
 	}
 
 
-	private function generateRefreshToken(\auth\models\User $user)
+	private function generateRefreshToken(User $user)
 	{
 		$refreshToken = Yii::$app->security->generateRandomString(200);
 		// TODO: Don't always regenerate - you could reuse existing one if user already has one with same IP and user agent
@@ -247,12 +206,7 @@ class AuthController extends \helpers\ApiController
 			]);
 		}
 		if (!$userRefreshToken->save(false)) {
-			// return $this->errorResponse(440);
-			return $this->errorResponse([
-				'statusCode' => [
-					'440'
-				]
-			]);
+			return $this->errorResponse(440);
 		}
 		// Send the refresh-token to the user in a HttpOnly cookie that Javascript can never read and that's limited by path
 		Yii::$app->response->cookies->add(new \yii\web\Cookie([
@@ -261,7 +215,7 @@ class AuthController extends \helpers\ApiController
 			'httpOnly' => true,
 			'sameSite' => 'none',
 			'secure' => true,
-			'path' => '/v1/auth/refresh',  //endpoint URI for renewing the JWT token using this refresh-token, or deleting refresh-token
+			'path' => '/v1/iam/auth/refresh',  //endpoint URI for renewing the JWT token using this refresh-token, or deleting refresh-token
 		]));
 		return $userRefreshToken;
 	}
--- modules/auth/models/User.php
+++ modules/auth/models/User.php
@@ -3,21 +3,26 @@
 namespace auth\models;
 
 use Yii;
-use yii\base\NotSupportedException;
+use helpers\traits\UserJWT;
 
 class User extends BaseModel implements \yii\web\IdentityInterface
 {
+    use UserJWT;
+    public $token;
+
     const STATUS_INACTIVE = 9;
     const STATUS_ACTIVE = 10;
+
     public static function tableName()
     {
         return '{{%users}}';
     }
+
     public function rules()
     {
         return [
             ['username', 'required'],
-            ['status', 'default', 'value' => self::STATUS_ACTIVE],
+            ['status', 'default', 'value' => self::STATUS_INACTIVE],
             ['status', 'in', 'range' => [self::STATUS_ACTIVE, self::STATUS_INACTIVE]],
         ];
     }
@@ -27,150 +32,88 @@ class User extends BaseModel implements \yii\web\IdentityInterface
         return static::findOne(['user_id' => $id, 'status' => self::STATUS_ACTIVE]);
     }
 
-    public static function findIdentityByAccessToken($token, $type = null)
+    public function fields()
     {
-        throw new NotSupportedException('"findIdentityByAccessToken" is not implemented.');
+        return ['username', 'token', 'updated_at', 'created_at'];
     }
 
-    /**
-     * Finds user by username
-     *
-     * @param string $username
-     * @return static|null
-     */
     public static function findByUsername($username)
     {
         return static::find()
-            ->where(['OR', ['username' => $username]])
-            //->where(['OR', ['username' => $username], ['email_address' => $username],['mobile_number' => "+254" . substr($username, -9)]])
+            //->select('username, password_hash, auth_key, created_at, updated_at')
+            //->where(['status' => self::STATUS_ACTIVE])
+            ->andWhere(['OR', ['username' => $username]])
             ->one();
     }
-    /**
-     * Finds user by password reset token
-     *
-     * @param string $token password reset token
-     * @return static|null
-     */
+    
     public static function findByPasswordResetToken($token)
     {
         if (!static::isPasswordResetTokenValid($token)) {
             return null;
         }
-
         return static::findOne([
             'password_reset_token' => $token,
             'status' => self::STATUS_ACTIVE,
+            'is_deleted' => FALSE,
         ]);
     }
-
-
-    /**
-     * Finds user by verification email token
-     *
-     * @param string $token verify email token
-     * @return static|null
-     */
-    public static function findByVerificationToken($token)
-    {
-        return static::findOne([
-            'verification_token' => $token,
-            'status' => self::STATUS_INACTIVE
-        ]);
-    }
-
-    /**
-     * Finds out if password reset token is valid
-     *
-     * @param string $token password reset token
-     * @return bool
-     */
+    
     public static function isPasswordResetTokenValid($token)
     {
         if (empty($token)) {
             return false;
         }
-
-        $timestamp = (int) substr($token, strrpos($token, '_') + 1);
-        $expire = 3600;
+        $timestamp = (int)substr($token, strrpos($token, '_') + 1);
+        $expire = Yii::$app->params['passwordResetTokenExpire'];
         return $timestamp + $expire >= time();
     }
 
-    /**
-     * {@inheritdoc}
-     */
+    public function afterFind()
+    {
+        parent::afterFind();
+        $this->token = $this->getJWT();
+        
+        $parse = Yii::$app->formatter;
+        $this->created_at = $parse->asDate($this->created_at, 'php:Y-m-d H:i:s');
+        $this->updated_at = $parse->asDate($this->updated_at, 'php:Y-m-d H:i:s');
+    }
+    
     public function getId()
     {
         return $this->getPrimaryKey();
     }
-
-    /**
-     * {@inheritdoc}
-     */
+    
     public function getAuthKey()
     {
         return $this->auth_key;
     }
-
-    /**
-     * {@inheritdoc}
-     */
+    
     public function validateAuthKey($authKey)
     {
         return $this->getAuthKey() === $authKey;
     }
-
-    /**
-     * Validates password
-     *
-     * @param string $password password to validate
-     * @return bool if password provided is valid for current user
-     */
+    
     public function validatePassword($password)
     {
         return Yii::$app->security->validatePassword($password, $this->password_hash);
     }
-
-    /**
-     * Generates password hash from password and sets it to the model
-     *
-     * @param string $password
-     */
+    
     public function setPassword($password)
     {
         $this->password_hash = Yii::$app->security->generatePasswordHash($password);
     }
-
-    /**
-     * Generates "remember me" authentication key
-     */
+    
     public function generateAuthKey()
     {
         $this->auth_key = Yii::$app->security->generateRandomString();
     }
-
-    /**
-     * Generates new password reset token
-     */
+    
     public function generatePasswordResetToken()
     {
-        return Yii::$app->security->generateRandomString() . '_' . time();
+        $this->password_reset_token = Yii::$app->security->generateRandomString() . '_' . time();
     }
-    /**
-     * Generates new token for email verification
-     */
-    public function generateEmailVerificationToken()
-    {
-        $this->verification_token = Yii::$app->security->generateRandomString() . '_' . time();
-    }
-    /**
-     * Removes password reset token
-     */
-    public function removePasswordResetToken()
-    {
-        $this->password_reset_token = null;
-    }
-    public function getProfile()
-    {
-        return $this->hasOne(Profiles::class, ['user_id' => 'user_id']);
+    public function getProfile(){
+        return $this;
+         // return $this->hasOne(Profiles::class, ['user_id' => 'user_id']);
     }
 }
