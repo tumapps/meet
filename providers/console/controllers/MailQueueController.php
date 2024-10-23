e98e8b2ee8e9b6aa264ede007638d594ecf1ee31
Francis Yuppie <dcs00922022@students.tum.ac.ke>
2024-10-23 12:56:32 +0300

    mail console controller

---
+++ providers/console/controllers/MailQueueController.php
@@ -0,0 +1,59 @@
+<?php
+namespace cmd\controllers;
+
+use yii\console\Controller;
+use Yii;
+use yii\console\ExitCode;
+use yii\helpers\Console; 
+use app\providers\components\MailQueueManager;
+
+class MailQueueController extends Controller {
+
+	private $queue;
+
+
+    public function init()
+    {
+        parent::init();
+        $this->queue = new MailQueueManager();
+    }
+
+    public function actionTest()
+    {
+    	$this->stdout("Mail queue is up");
+    }
+
+    public function actionRun()
+    {
+    	while(true){
+    		$this->queue->processQueue();
+    		sleep(1);
+    	}
+    }
+
+    public function actionViewQueue()
+	{
+	    $queueList = $this->queue->viewQueue();
+	    if (empty($queueList)) {
+	        $this->stdout("The email queue is empty.\n", Console::FG_GREEN);
+	        return ExitCode::OK; 
+	    }
+	    foreach ($queueList as $item) {
+	        $emailData = unserialize($item);
+	        $this->stdout("Email: {$emailData['email']}, Subject: {$emailData['subject']}\n");
+	    }
+	    return ExitCode::OK;
+	}
+
+    public function actionRemoveFromQueue($email)
+	{
+	    if ($this->queue->removeFromQueue($email)) {
+	        $this->stdout("Email '{$email}' removed from the queue.\n", Console::FG_GREEN);
+	    } else {
+	        $this->stdout("Failed to remove email '{$email}' from the queue.\n", Console::FG_RED);
+	    }
+	    return ExitCode::OK;
+	}
+
+
+}
