c60cf524410c6ba220731024ccc919f8689ad8f3
Francis Yuppie <dcs00922022@students.tum.ac.ke>
2024-11-08 14:21:37 +0300

    add approve, reject and pendingAppoiments endpoints routes

--- modules/scheduler/routers/AppointmentsRouter.php
+++ modules/scheduler/routers/AppointmentsRouter.php
@@ -49,6 +49,53 @@ return [
 'GET appointments'     => 'appointments/index',
 
 /**
+ * @OA\Get(path="/scheduler/pending-appointments",
+ *   summary="Lists all Pending Appointments models ",
+ *   tags={"Appointments"},
+ *   @OA\Parameter(description="Page No.",in="query",name="page", @OA\Schema(type="integer")),
+ *   @OA\Parameter(description="Page Size",in="query",name="per-page", @OA\Schema(type="integer")),
+ *   @OA\Parameter(description="Search",in="query",name="_search", @OA\Schema(type="string")),
+ *
+  *    @OA\Parameter(description="Id",in="query",name="_id", @OA\Schema(type="integer")),
+ *    @OA\Parameter(description="User Id",in="query",name="_user_id", @OA\Schema(type="bigint")),
+ *    @OA\Parameter(description="Date",in="query",name="_date", @OA\Schema(type="date")),
+ *    @OA\Parameter(description="Start Time",in="query",name="_start_time", @OA\Schema(type="time")),
+ *    @OA\Parameter(description="End Time",in="query",name="_end_time", @OA\Schema(type="time")),
+ *    @OA\Parameter(description="Contact Name",in="query",name="_contact_name", @OA\Schema(type="string")),
+ *    @OA\Parameter(description="Email Address",in="query",name="_email_address", @OA\Schema(type="string")),
+ *    @OA\Parameter(description="Mobile Number",in="query",name="_mobile_number", @OA\Schema(type="string")),
+ *    @OA\Parameter(description="Subject",in="query",name="_subject", @OA\Schema(type="text")),
+ *    @OA\Parameter(description="Appointment Type",in="query",name="_appointment_type", @OA\Schema(type="string")),
+ *    @OA\Parameter(description="Status",in="query",name="_status", @OA\Schema(type="string")),
+ *    @OA\Parameter(description="Created At",in="query",name="_created_at", @OA\Schema(type="timestamp")),
+ *    @OA\Parameter(description="Updated At",in="query",name="_updated_at", @OA\Schema(type="timestamp")),
+ *
+ *   @OA\Response(
+ *     response=200,
+ *     description="Returns a data payload object for all scheduler/appointments",
+ *      @OA\JsonContent(
+ *          @OA\Property(property="dataPayload", type="object",
+ *              @OA\Property(property="data", type="array",@OA\Items(ref="#/components/schemas/Appointments")),
+ *              @OA\Property(property="countOnPage", type="integer", example="25"),
+ *              @OA\Property(property="totalCount", type="integer",example="50"),
+ *              @OA\Property(property="perPage", type="integer",example="25"),
+ *              @OA\Property(property="totalPages", type="integer",example="2"),
+ *              @OA\Property(property="currentPage", type="integer",example="1"),
+ *              @OA\Property(property="paginationLinks", type="object",
+ *                  @OA\Property(property="first", type="string",example="v1/v1/scheduler/appointments?page=1&per-page=25"),
+ *                  @OA\Property(property="previous", type="string",example="v1/v1/scheduler/appointments?page=1&per-page=25"),
+ *                  @OA\Property(property="self", type="string",example="v1/v1/scheduler/appointments?page=1&per-page=25"),
+ *                  @OA\Property(property="next", type="string",example="v1/v1/scheduler/appointments?page=1&per-page=25"),
+ *                  @OA\Property(property="last", type="string",example="v1/v1/scheduler/appointments?page=1&per-page=25"),
+ *              ),
+ *          )
+ *      )
+ *   ),
+ * )
+ */
+'GET pending-appointments' => 'appointments/pending-appointments',
+
+/**
  * @OA\Post(
  * path="/scheduler/get-slots",
  * summary="Get all available slots ",
@@ -271,6 +318,62 @@ return [
 * )
 */
 'PUT appointments/{id}'     => 'appointments/update',
+
+/**
+* @OA\Put(
+*     path="/scheduler/aapprove/{id}",
+*     tags={"Appointments"},
+*     summary="Approve an existing Appointments model with a status pending",
+*     @OA\Parameter(description="Appointments unique ID to load and update",in="path",name="id",required=true,@OA\Schema(type="string",)),
+*     @OA\RequestBody(
+*        required=true,
+*        description="Finds the Appointments model to be cancelled based on its primary key value",
+*        @OA\JsonContent(
+*           ref="#/components/schemas/Appointments",
+*        ),
+*     ),
+*    @OA\Response(
+*       response=202,
+*       description="Data payload",
+*       @OA\JsonContent(
+*          @OA\Property(property="dataPayload", type="object",
+*             @OA\Property(property="data", type="object",ref="#/components/schemas/Appointments"),
+*             @OA\Property(property="toastMessage", type="string", example="appointments cancelled succefully"),
+*             @OA\Property(property="toastTheme", type="string",example="success"),
+*          )
+*       )
+*    ),
+* )
+*/
+'PUT approve/{id}' => 'appointments/approve',
+
+/**
+* @OA\Put(
+*     path="/scheduler/reject/{id}",
+*     tags={"Appointments"},
+*     summary="reject an existing Appointments model with a status pending",
+*     @OA\Parameter(description="Appointments unique ID to load and update",in="path",name="id",required=true,@OA\Schema(type="string",)),
+*     @OA\RequestBody(
+*        required=true,
+*        description="Finds the Appointments model to be cancelled based on its primary key value",
+*        @OA\JsonContent(
+*           ref="#/components/schemas/Appointments",
+*        ),
+*     ),
+*    @OA\Response(
+*       response=202,
+*       description="Data payload",
+*       @OA\JsonContent(
+*          @OA\Property(property="dataPayload", type="object",
+*             @OA\Property(property="data", type="object",ref="#/components/schemas/Appointments"),
+*             @OA\Property(property="toastMessage", type="string", example="appointments cancelled succefully"),
+*             @OA\Property(property="toastTheme", type="string",example="success"),
+*          )
+*       )
+*    ),
+* )
+*/
+'PUT reject/{id}' => 'appointments/reject',
 
 /**
 * @OA\Put(
