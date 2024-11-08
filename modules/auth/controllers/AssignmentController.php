840f40abdb7892d7374cde7e7ce44e9816467710
Francis Yuppie <dcs00922022@students.tum.ac.ke>
2024-11-08 23:48:51 +0300

    assignment controller

--- modules/auth/controllers/AssignmentController.php
+++ modules/auth/controllers/AssignmentController.php
@@ -4,69 +4,129 @@ namespace auth\controllers;
 
 use Yii;
 use auth\models\Assignment;
+use auth\models\User;
 use auth\models\searches\AssignmentSearch;
 
 
 class AssignmentController extends \helpers\ApiController {
 
-	public function actionIndex()
-	{
-		$searchModel = new AssignmentSearch();
-        $search = $this->queryParameters(Yii::$app->request->queryParams,'AssignmentSearch');
-        $dataProvider = $searchModel->search($search);
-        return $this->payloadResponse($dataProvider,['oneRecord'=>false]);
-	}
-
-	public function actionView($id)
+	/**
+    * Assigns a permission to a role
+    * @param string $roleName
+    * @param string $permissionName
+    * @return \yii\web\Response
+    */
+    public function actionPermissionToRole()
     {
-        // Yii::$app->user->can('');
-        return $this->payloadResponse($this->findModel($id));
-    }
-
-
-	public function actionCreate()
-    {
-        // Yii::$app->user->can('assignRole');
-        $model = new Assignment();
-        $model->loadDefaultValues();
         $dataRequest['Assignment'] = Yii::$app->request->getBodyParams();
-        if($model->load($dataRequest) && $model->save()) {
-            return $this->payloadResponse($model,['statusCode'=>201,'message'=>'Record added successfully']);
+
+        $roleName = $dataRequest['Assignment']['role'];
+        $permissionName = $dataRequest['Assignment']['permission'];
+
+        if(empty($roleName) || empty($permissionName)) {
+            return $this->errorResponse(['message' => ['Role and Permission name is required']]);
         }
-        return $this->errorResponse($model->getErrors()); 
+
+        $auth = Yii::$app->authManager;
+        
+        $role = $auth->getRole($roleName);
+        $permission = $auth->getPermission($permissionName);
+        
+        if (!$role || !$permission) {
+            return $this->errorResponse(['message' => ['Role or Permission not found.']]);
+        }
+        
+        $auth->addChild($role, $permission);
+         
+        return $this->toastResponse(['statusCode'=>202,'message'=>"Permission '{$permissionName}' assigned to role '{$roleName}'."]);
     }
 
-    public function actionUpdate($id)
+    /**
+     * Assigns a role to another role (parent role and child role)
+     * @param string $parentRoleName
+     * @param string $childRoleName
+     * @return \yii\web\Response
+     */
+    public function actionRoleToRole()
     {
-        // Yii::$app->user->can('updateRoleAssignment');
         $dataRequest['Assignment'] = Yii::$app->request->getBodyParams();
-        $model = $this->findModel($id);
-        if($model->load($dataRequest) && $model->save()) {
-           return $this->payloadResponse($this->findModel($id),['statusCode'=>202,'message'=>'Record updated successfully']);
+
+        $childRoleName = $dataRequest['Assignment']['child_role'];
+        $parentRoleName = $dataRequest['Assignment']['parent_role'];
+
+        if(empty($childRoleName) || empty($childRoleName)) {
+            return $this->errorResponse(['message' => ['Child and Parent Role is required']]);
         }
-        return $this->errorResponse($model->getErrors()); 
+
+        $auth = Yii::$app->authManager;
+
+        $parentRole = $auth->getRole($parentRoleName);
+        $childRole = $auth->getRole($childRoleName);
+
+        if (!$parentRole || !$childRole) {
+            return $this->errorResponse(['message' => ['Parent Role or Child Role not found.']]);
+        }
+
+        $auth->addChild($parentRole, $childRole);
+        return $this->toastResponse(['statusCode'=>202,'message'=>"Role '{$childRoleName}' assigned to role '{$parentRoleName}'."]);
     }
 
-    public function actionDelete($id)
+    /**
+     * Assigns a role to a user
+     * @param string $userId
+     * @param string $roleName
+     * @return \yii\web\Response
+     */
+    public function actionRoleToUser()
     {
-        $model = $this->findModel($id);
-        if ($model->is_deleted) {
-            // Yii::$app->user->can('RoleAssignmentRestore');
-            $model->restore();
-            return $this->toastResponse(['statusCode'=>202,'message'=>'Record restored successfully']);
-        } else {
-            // Yii::$app->user->can('RoleAssignmentDelete');
-            $model->delete();
-            return $this->toastResponse(['statusCode'=>202,'message'=>'Record deleted successfully']);
+        $dataRequest['Assignment'] = Yii::$app->request->getBodyParams();
+
+        $userId = $dataRequest['Assignment']['user_id'];
+        $roleName = $dataRequest['Assignment']['role_name'];
+
+        if(empty($userId) || empty($roleName)) {
+            return $this->errorResponse(['message' => ['User Id and Role is required']]);
         }
-        return $this->errorResponse($model->getErrors()); 
+
+        if(!is_numeric($userId)){
+           return $this->errorResponse(['message' => ['User id must be an integer']]); 
+        }
+
+        $auth = Yii::$app->authManager;
+
+        $role = $auth->getRole($roleName);
+
+        if (!$role) {
+            return $this->errorResponse(['message' => ['Role not found.']]);
+        }
+
+        $id = User::findOne(['user_id' => (int)$userId]);
+
+        if (!$id) {
+            return $this->errorResponse(['message' => ['Provided user id does not exists']]);
+        }
+
+        $auth->assign($role, $userId);
+        return $this->toastResponse(['statusCode'=>202,'message'=>"Role '{$roleName}' assigned to user with ID '{$userId}'."]);
     }
 
-    protected function findModel($id)
+    /**
+     * Lists all roles, permissions, and their assignments
+     * @return \yii\web\Response
+     */
+    public function actionListAssignments()
     {
-        if (($model = Assignment::findOne(['id' => $id])) !== null) {
-            return $model;
-        }
-        throw new \yii\web\NotFoundHttpException('Record not found.');
+        $auth = Yii::$app->authManager;
+
+        $roles = $auth->getRoles();
+        $permissions = $auth->getPermissions();
+
+        $assignments = [
+            'roles' => array_keys($roles),
+            'permissions' => array_keys($permissions),
+        ];
+
+        return $this->payloadResponse($assignments);
     }
+     
 }
