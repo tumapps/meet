3004d77b5b9cd17cad5d38f06d86a034a43a1298
Francis-Yuppie <dcs00922022@students.tum.ac.ke>
2024-12-15 23:26:45 +0300

    update date formart rule syntax

--- modules/scheduler/controllers/EventsController.php
+++ modules/scheduler/controllers/EventsController.php
@@ -5,27 +5,29 @@ namespace scheduler\controllers;
 use Yii;
 use scheduler\models\Events;
 use scheduler\models\searches\EventsSearch;
+
 /**
  * @OA\Tag(
  *     name="Events",
  *     description="Available endpoints for Events model"
  * )
  */
-class EventsController extends \helpers\ApiController{
+class EventsController extends \helpers\ApiController
+{
     public $permissions = [
-        'schedulerEventsList'=>'View Events List',
-        'schedulerEventsCreate'=>'Add Events',
-        'schedulerEventsUpdate'=>'Edit Events',
-        'schedulerEventsDelete'=>'Delete Events',
-        'schedulerEventsRestore'=>'Restore Events',
-        ];
+        'schedulerEventsList' => 'View Events List',
+        'schedulerEventsCreate' => 'Add Events',
+        'schedulerEventsUpdate' => 'Edit Events',
+        'schedulerEventsDelete' => 'Delete Events',
+        'schedulerEventsRestore' => 'Restore Events',
+    ];
     public function actionIndex()
     {
         Yii::$app->user->can('schedulerEventsList');
         $searchModel = new EventsSearch();
-        $search = $this->queryParameters(Yii::$app->request->queryParams,'EventsSearch');
+        $search = $this->queryParameters(Yii::$app->request->queryParams, 'EventsSearch');
         $dataProvider = $searchModel->search($search);
-        return $this->payloadResponse($dataProvider,['oneRecord'=>false]);
+        return $this->payloadResponse($dataProvider, ['oneRecord' => false]);
     }
 
     public function actionView($id)
@@ -37,11 +39,11 @@ class EventsController extends \helpers\ApiController{
     public function actionCancel($id)
     {
         Yii::$app->user->can('registrar');
-        
+
         $request = Yii::$app->request;
 
         $model = $this->findModel($id);
-    
+
         // Set scenario to 'cancel' for validation
         $model->scenario = Events::SCENARIO_CANCEL;
 
@@ -59,22 +61,24 @@ class EventsController extends \helpers\ApiController{
 
         $model->status = Events::STATUS_CANCELLED;
 
-        if($model->save(false)){
-            return $this->toastResponse(['statusCode'=>202,'message'=>'Event CANCELLED successfully']);
+        if ($model->save(false)) {
+            return $this->toastResponse(['statusCode' => 202, 'message' => 'Event CANCELLED successfully']);
         }
-        return $this->errorResponse($model->getErrors()); 
+        return $this->errorResponse($model->getErrors());
     }
 
     public function actionCreate()
     {
-        Yii::$app->user->can('registrar');
-        $model = new Events();
-        $model->loadDefaultValues();
-        $dataRequest['Events'] = Yii::$app->request->getBodyParams();
-        if($model->load($dataRequest) && $model->save()) {
-            return $this->payloadResponse($model,['statusCode'=>201,'message'=>'Events added successfully']);
+        // Yii::$app->user->can('registrar');
+        if (Yii::$app->user->can('registrar') || Yii::$app->user->can('superuser')) {
+            $model = new Events();
+            $model->loadDefaultValues();
+            $dataRequest['Events'] = Yii::$app->request->getBodyParams();
+            if ($model->load($dataRequest) && $model->save()) {
+                return $this->payloadResponse($model, ['statusCode' => 201, 'message' => 'Events added successfully']);
+            }
+            return $this->errorResponse($model->getErrors());
         }
-        return $this->errorResponse($model->getErrors()); 
     }
 
     public function actionUpdate($id)
@@ -82,10 +86,10 @@ class EventsController extends \helpers\ApiController{
         Yii::$app->user->can('registrar');
         $dataRequest['Events'] = Yii::$app->request->getBodyParams();
         $model = $this->findModel($id);
-        if($model->load($dataRequest) && $model->save()) {
-           return $this->payloadResponse($this->findModel($id),['statusCode'=>202,'message'=>'Events updated successfully']);
+        if ($model->load($dataRequest) && $model->save()) {
+            return $this->payloadResponse($this->findModel($id), ['statusCode' => 202, 'message' => 'Events updated successfully']);
         }
-        return $this->errorResponse($model->getErrors()); 
+        return $this->errorResponse($model->getErrors());
     }
 
     public function actionDelete($id)
@@ -94,13 +98,13 @@ class EventsController extends \helpers\ApiController{
         if ($model->is_deleted) {
             Yii::$app->user->can('registrar');
             $model->restore();
-            return $this->toastResponse(['statusCode'=>202,'message'=>'Events restored successfully']);
+            return $this->toastResponse(['statusCode' => 202, 'message' => 'Events restored successfully']);
         } else {
             Yii::$app->user->can('registrar');
             $model->delete();
-            return $this->toastResponse(['statusCode'=>202,'message'=>'Events deleted successfully']);
+            return $this->toastResponse(['statusCode' => 202, 'message' => 'Events deleted successfully']);
         }
-        return $this->errorResponse($model->getErrors()); 
+        return $this->errorResponse($model->getErrors());
     }
 
     protected function findModel($id)
