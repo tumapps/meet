11db269a139907c94a7825b1c92f73c1652ea4f3
Francis-Yuppie <dcs00922022@students.tum.ac.ke>
2024-12-17 15:06:32 +0300

    add report controller

---
+++ modules/scheduler/controllers/ReportsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace scheduler\controllers;
+
+
+class ReportsController extends \helpers\ApiController
+{
+    public function actionSystemWide() {}
+
+    public function actionUserReports($user_id)
+    {
+        // Placeholder for user-level report logic
+    }
+
+    public function actionSummary($filter = 'monthly')
+    {
+        // Placeholder for summary report logic
+    }
+    
+}
