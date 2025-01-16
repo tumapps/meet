4e2f4f234147921396fe3475f48da0f2ff99faf0
Francis-Yuppie <dcs00922022@students.tum.ac.ke>
2025-01-16 13:58:27 +0300

    updaqtes in the action run

--- providers/console/controllers/MailQueueController.php
+++ providers/console/controllers/MailQueueController.php
@@ -1,59 +1,60 @@
 <?php
+
 namespace cmd\controllers;
 
 use yii\console\Controller;
 use Yii;
 use yii\console\ExitCode;
-use yii\helpers\Console; 
+use yii\helpers\Console;
 use app\providers\components\MailQueueManager;
 
-class MailQueueController extends Controller {
+class MailQueueController extends Controller
+{
 
 	private $queue;
 
 
-    public function init()
-    {
-        parent::init();
-        $this->queue = new MailQueueManager();
-    }
-
-    public function actionTest()
-    {
-    	$this->stdout("Mail queue is up");
-    }
-
-    public function actionRun()
-    {
-    	while(true){
-    		$this->queue->processQueue();
-    		sleep(1);
-    	}
-    }
-
-    public function actionViewQueue()
+	public function init()
 	{
-	    $queueList = $this->queue->viewQueue();
-	    if (empty($queueList)) {
-	        $this->stdout("The email queue is empty.\n", Console::FG_GREEN);
-	        return ExitCode::OK; 
-	    }
-	    foreach ($queueList as $item) {
-	        $emailData = unserialize($item);
-	        $this->stdout("Email: {$emailData['email']}, Subject: {$emailData['subject']}\n");
-	    }
-	    return ExitCode::OK;
+		parent::init();
+		$this->queue = new MailQueueManager();
 	}
 
-    public function actionRemoveFromQueue($email)
+	public function actionTest()
 	{
-	    if ($this->queue->removeFromQueue($email)) {
-	        $this->stdout("Email '{$email}' removed from the queue.\n", Console::FG_GREEN);
-	    } else {
-	        $this->stdout("Failed to remove email '{$email}' from the queue.\n", Console::FG_RED);
-	    }
-	    return ExitCode::OK;
+		$this->stdout("Mail queue is up");
 	}
 
+	public function actionRun()
+	{
+		while (true) {
+			$this->queue->processQueue();
+			$this->queue->retryFailedEmails();
+			sleep(1);
+		}
+	}
 
+	public function actionViewQueue()
+	{
+		$queueList = $this->queue->viewQueue();
+		if (empty($queueList)) {
+			$this->stdout("The email queue is empty.\n", Console::FG_GREEN);
+			return ExitCode::OK;
+		}
+		foreach ($queueList as $item) {
+			$emailData = unserialize($item);
+			$this->stdout("Email: {$emailData['email']}, Subject: {$emailData['subject']}\n");
+		}
+		return ExitCode::OK;
+	}
+
+	public function actionRemoveFromQueue($email)
+	{
+		if ($this->queue->removeFromQueue($email)) {
+			$this->stdout("Email '{$email}' removed from the queue.\n", Console::FG_GREEN);
+		} else {
+			$this->stdout("Failed to remove email '{$email}' from the queue.\n", Console::FG_RED);
+		}
+		return ExitCode::OK;
+	}
 }
