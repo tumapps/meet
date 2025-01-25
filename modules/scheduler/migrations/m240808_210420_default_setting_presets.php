9d457770f8aa0ec8f95dd23edd3f3e8b43cdf888
Francis-Yuppie <dcs00922022@students.tum.ac.ke>
2025-01-25 10:56:25 +0300

    add system migration table

--- modules/scheduler/migrations/m240808_210420_default_setting_presets.php
+++ modules/scheduler/migrations/m240808_210420_default_setting_presets.php
@@ -16,7 +16,8 @@ class m240808_210420_default_setting_presets extends Migration
 
         $this->createTable('{{%settings}}', [
             'id' => $this->primaryKey(),
-            'user_id' => $this->bigInteger(),
+            'user_id' => $this->bigInteger()->unique(),
+            // 'data' => $this->json()->notNull(),
             'start_time' => $this->time()->notNull(),
             'end_time' => $this->time()->notNull(),
             'slot_duration' => $this->integer()->notNull()->defaultValue(30), // minutes
@@ -25,10 +26,28 @@ class m240808_210420_default_setting_presets extends Migration
             'reminder_time' => $this->integer()->notNull()->defaultValue(30), // minutes
             'created_at' => $this->integer(),
             'updated_at' => $this->integer(),
-            'is_deleted' => $this->boolean()->defaultValue(false),
+            'is_deleted' => $this->integer()->defaultValue(0),
             'FOREIGN KEY ([[user_id]]) REFERENCES {{%users}} ([[user_id]])' .
                 $this->buildFkClause('ON DELETE CASCADE', 'ON UPDATE CASCADE'),
         ],  $tableOptions);
+
+        $this->createTable('{{%system_settings}}', [
+            'id' => $this->primaryKey(),
+            'app_name' => $this->string(255)->null(),
+            'system_email' =>  $this->string(128)->notNull(),
+            'category' => $this->string(20)->notNull()->defaultValue('GENERAL'),
+            'email_scheme' => $this->string(128)->notNull(),
+            'email_smtps' => $this->string(128)->notNull(),
+            'email_port' => $this->integer()->notNull(),
+            'email_encryption' => $this->string(10)->notNull(),
+            'email_password' => $this->string(128)->notNull(),
+            'description' => $this->text()->null(), // description column can be useful for storing metadata or explanations about each setting.
+            'email_username' => $this->string(128)->notNull(),
+            'updated_at' => $this->integer(),
+            'created_at' => $this->integer(),
+            'updated_at' => $this->integer(),
+            'is_deleted' => $this->boolean()->defaultValue(false),
+        ]);
     }
 
     /**
@@ -36,7 +55,8 @@ class m240808_210420_default_setting_presets extends Migration
      */
     public function safeDown()
     {
-         $this->dropTable('{{%settings}}');
+        $this->dropTable('{{%system_settings}}');
+        $this->dropTable('{{%settings}}');
     }
 
     protected function buildFkClause($delete = '', $update = '')
