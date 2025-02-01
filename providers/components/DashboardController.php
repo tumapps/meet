6a6a33ecea5236a6db655989a1b4cf048f506544
Francis-Yuppie <dcs00922022@students.tum.ac.ke>
2025-02-01 11:24:15 +0300

    add cors filters

--- providers/components/DashboardController.php
+++ providers/components/DashboardController.php
@@ -2,12 +2,41 @@
 
 namespace helpers;
 
+use yii\filters\Cors;
+use yii\filters\auth\HttpBearerAuth;
 
 class DashboardController extends  \yii\web\Controller
 {
     public $layout = 'dashboard';
     public function behaviors()
     {
+        $auth     = isset(\Yii::$app->params['activateAuth']) ? \Yii::$app->params['activateAuth'] : FALSE;
+        $origins  = isset(\Yii::$app->params['allowedDomains']) ? \Yii::$app->params['allowedDomains'] : "*";
+        
+        $behavior = [
+            'class' => Cors::class,
+            'cors'  => [
+                'Origin'                           => [$origins],
+                'Access-Control-Allow-Origin'      => [$origins],
+                'Access-Control-Request-Headers'   => ['*'],
+                'Access-Control-Request-Method'    => ['POST', 'PUT', 'PATCH', 'GET', 'DELETE', 'HEAD'],
+                'Access-Control-Allow-Credentials' => true,
+                'Access-Control-Max-Age'           => 3600,
+            ],
+        ];
+
+        if ($auth) {
+            $behaviors = parent::behaviors();
+            unset($behaviors['authenticator']);
+            $behaviors['corsFilter'] = $behavior;
+            $behaviors['authenticator'] = [
+                'class' => HttpBearerAuth::className(),
+            ];
+            $behaviors['authenticator']['except'] = \Yii::$app->params['safeEndpoints'];
+        } else {
+            $behaviors['corsFilter'] = $behavior;
+        }
+
         $access = [
             'as access' => [
                 'class' => \yii\filters\AccessControl::class,
@@ -23,6 +52,6 @@ class DashboardController extends  \yii\web\Controller
                 ],
             ],
         ];
-        return array_merge(parent::behaviors(), $access);
+        return array_merge(parent::behaviors(), $access, $behavior);
     }
 }
