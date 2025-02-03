aaa76f30e33388aa0491653da81a12a38916f203
Francis-Yuppie <dcs00922022@students.tum.ac.ke>
2025-02-03 16:04:27 +0300

    fixed user_id error in action update

--- modules/scheduler/controllers/SpaceController.php
+++ modules/scheduler/controllers/SpaceController.php
@@ -104,7 +104,7 @@ class SpaceController extends \helpers\ApiController
         $dataRequest['Space'] = Yii::$app->request->getBodyParams();
         $model = $this->findModel($id);
 
-        $roleFlags = $this->getRoleFlags(Yii::$app->user->user_id);
+        $roleFlags = $this->getRoleFlags(Yii::$app->user->id);
 
         if ($roleFlags['isUser']) {
             if ($model->id !== Yii::$app->user->user_id || $model->space_type !== Space::SPACE_TYPE_UNMANAGED) {
