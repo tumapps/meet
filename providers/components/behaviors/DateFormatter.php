571e972777453eb33c9349d6f341342d34ff5def
Francis-Yuppie <dcs00922022@students.tum.ac.ke>
2025-02-03 16:07:01 +0300

    updated behavior

--- providers/components/behaviors/DateFormatter.php
+++ providers/components/behaviors/DateFormatter.php
@@ -16,15 +16,8 @@ class DateFormatter extends Behavior
 
     public function formatDates($event)
     {
-        // if ($this->owner->hasAttribute('created_at')) {
-        //     $this->owner->created_at = Date('d M Y, H:i', $this->owner->created_at);
-        //     // $this->owner->created_at = Yii::$app->formatter->asRelativeTime($this->owner->created_at);
-        //     // $this->owner->created_at = Yii::$app->formatter->asDateTime($this->owner->created_at, 'php: M Y, H:i');
-
-        // }
-
         if ($this->owner->hasAttribute('appointment_date')) {
-            $this->owner->appointment_date = Date('d M Y', strtotime($this->owner->appointment_date));
+            $this->owner->appointment_date = date('Y-m-d', strtotime($this->owner->appointment_date));
         }
 
         if ($this->owner->hasAttribute('start_time')) {
