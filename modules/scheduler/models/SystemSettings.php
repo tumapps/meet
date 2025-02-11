fd99f8021999c4e041da1b216da203a818e49620
Francis-Yuppie <dcs00922022@students.tum.ac.ke>
2025-02-12 00:25:38 +0300

    complete smpt validation test

--- modules/scheduler/models/SystemSettings.php
+++ modules/scheduler/models/SystemSettings.php
@@ -42,56 +42,56 @@ class SystemSettings extends BaseModel
     /**
      * list of fields to output by the payload.
      */
-    public function fields()
-    {
-        return array_merge(
-            parent::fields(),
-            [
-                'id',
-                'app_name',
-                'system_email',
-                'category',
-                'email_scheme',
-                'email_smtps',
-                'email_port',
-                'email_encryption',
-                'email_password',
-                'description',
-                'email_username',
-                'updated_at',
-                'created_at',
-                'is_deleted',
-            ]
-        );
-    }
     // public function fields()
     // {
-    //     $fields = parent::fields();
-
-    //     $customFields = [
-    //         'id',
-    //         'app_name',
-    //         'system_email',
-    //         'category',
-    //         'email_scheme',
-    //         'email_smtps',
-    //         'email_port',
-    //         'email_encryption',
-    //         'description',
-    //         'email_username',
-    //         'updated_at',
-    //         'created_at',
-    //         'is_deleted',
-    //     ];
-
-    //     unset($fields['email_password']);
-
-    //     $customFields['decrypted_email_password'] = function () {
-    //         return '[PROTECTED]';
-    //     };
-
-    //     return array_merge($fields, $customFields);
+    //     return array_merge(
+    //         parent::fields(),
+    //         [
+    //             'id',
+    //             'app_name',
+    //             'system_email',
+    //             'category',
+    //             'email_scheme',
+    //             'email_smtps',
+    //             'email_port',
+    //             'email_encryption',
+    //             'email_password',
+    //             'description',
+    //             'email_username',
+    //             'updated_at',
+    //             'created_at',
+    //             'is_deleted',
+    //         ]
+    //     );
     // }
+    public function fields()
+    {
+        $fields = parent::fields();
+
+        $customFields = [
+            'id',
+            'app_name',
+            'system_email',
+            'category',
+            'email_scheme',
+            'email_smtps',
+            'email_port',
+            'email_encryption',
+            'description',
+            'email_username',
+            'updated_at',
+            'created_at',
+            'is_deleted',
+        ];
+
+        unset($fields['email_password']);
+
+        $customFields['decrypted_email_password'] = function () {
+            return '[PROTECTED]';
+        };
+
+        return array_merge($fields, $customFields);
+    }
 
     /**
      * {@inheritdoc}
@@ -101,8 +101,8 @@ class SystemSettings extends BaseModel
         return [
             [['system_email', 'email_scheme', 'email_smtps', 'email_port', 'email_encryption', 'email_password', 'email_username'], 'required'],
             [['system_email'], 'email'],
-            [['email_port', 'updated_at', 'created_at'], 'default', 'value' => null],
-            [['email_port', 'updated_at', 'created_at'], 'integer'],
+            [['email_port'], 'default', 'value' => null],
+            [['email_port'], 'integer'],
             [['description'], 'string'],
             [['is_deleted'], 'boolean'],
             [['app_name'], 'string', 'max' => 255],
@@ -139,32 +139,51 @@ class SystemSettings extends BaseModel
 
     public function beforeSave($insert)
     {
-        if (!empty($this->email_password)) {
-            if (!password_get_info($this->email_password)['algo']) {
-                $this->email_password = password_hash($this->email_password, PASSWORD_BCRYPT);
-            }
-        }
+        try {
+            $result = parent::beforeSave($insert);
 
-        return parent::beforeSave($insert);
+            // if (!empty($this->email_password)) {
+            //     if (!password_get_info($this->email_password)['algo']) {
+            //         $this->email_password = password_hash($this->email_password, PASSWORD_BCRYPT);
+            //     }
+            // }
+
+            if (!$this->hasErrors()) {
+                return $result;
+            } else {
+                throw new \Exception("Validation Failed!");
+            }
+        } catch (\Exception $e) {
+            return false;
+        }
     }
 
 
     public function validateSmtpConnection($attribute, $params)
     {
         try {
-            $mailer = \Yii::$app->mailer;
+            $transport = new \Symfony\Component\Mailer\Transport\Smtp\EsmtpTransport(
+                $this->email_smtps,
+                $this->email_port,
+                $this->email_encryption === 'none' ? null : $this->email_encryption
+            );
 
-            $message = $mailer->compose()
-                ->setTo($this->system_email)
-                ->setFrom([$this->system_email])
-                ->setSubject('SMTP Validation Test')
-                ->setTextBody('Testing SMTP credentials.');
+            $transport->setUsername($this->email_username);
+            $transport->setPassword($this->email_password);
 
-            if (!$message->send()) {
-                throw new \Exception('SMTP validation failed. Unable to send test email.');
-            }
-        } catch (\Throwable $e) {
+            $mailer = new \Symfony\Component\Mailer\Mailer($transport);
+
+            $message = (new \Symfony\Component\Mime\Email())
+                ->from($this->system_email)
+                ->to($this->system_email)
+                ->subject('SMTP Validation Test')
+                ->text('Testing SMTP credentials.');
+
+            $mailer->send($message);
+        } catch (\Symfony\Component\Mailer\Exception\TransportExceptionInterface $e) {
             $this->addError($attribute, 'SMTP connection failed: ' . $e->getMessage());
+        } catch (\Throwable $e) {
+            $this->addError($attribute, 'An error occurred while validating SMTP credentials: ' . $e->getMessage());
         }
     }
 }
