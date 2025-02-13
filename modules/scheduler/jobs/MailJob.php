2e67fc5bb77c15e35852093a5df6efb1e7922ccd
Francis-Yuppie <dcs00922022@students.tum.ac.ke>
2025-02-13 19:04:55 +0300

    add new mail job class

---
+++ modules/scheduler/jobs/MailJob.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace scheduler\jobs;
+
+use Yii;
+use yii\base\BaseObject;
+use helpers\traits\Mail;
+use scheduler\models\Appointments;
+
+class MailJob extends BaseObject implements \yii\queue\JobInterface
+{
+    use Mail;
+
+    public $email;
+    public $subject;
+    public $body;
+    public $type;
+    public $id;
+
+    public function execute($queue)
+    {
+        $date = date('Y-m-d H:i:s');
+
+        if (self::send($this->email, $this->subject, $this->body)) {
+            if ($this->type === 'reminder' && $this->id !== null || !empty($this->id)) {
+                Appointments::updateReminder($this->id);
+            }
+            Yii::info("Email sent to {$this->email} at: {$date}", 'mailQueue');
+        } else {
+            Yii::error("Failed to send email to {$this->email}", 'mailQueue');
+
+            Yii::$app->queue->delay(30)->push(new MailJob([
+                'email' => $this->email,
+                'subject' => $this->subject,
+                'body' => $this->body,
+                'type' => $this->type,
+                'id' => $this->id,
+            ]));
+        }
+    }
+}
