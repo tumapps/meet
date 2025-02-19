9bf4ced9a5b1b0257d234e74385ea9f2eb7418b5
Francis-Yuppie <dcs00922022@students.tum.ac.ke>
2025-02-19 18:34:50 +0300

    new console  action

--- providers/console/controllers/AppointmentController.php
+++ providers/console/controllers/AppointmentController.php
@@ -4,50 +4,73 @@ namespace cmd\controllers;
 
 use Yii;
 use yii\console\Controller;
-use yii\console\ExitCode;
 use scheduler\models\Appointments;
 use yii\helpers\Console;
 
 
 class AppointmentController extends Controller
 {
-	public function actionHello()
-	{
-		$this->stdout('Hello');
-	}
+    private $appointmentModel;
 
-	public function actionCheckPassedAppointments()
+    public function init()
+    {
+        parent::init();
+        $this->appointmentModel = new Appointments();
+    }
+
+    public function actionRun()
+    {
+        Console::output("Starting appointment cron jobs...\n");
+
+        $this->actionSendAppointmentReminders();
+        $this->actionAppointmentsPastOneHour();
+
+        Console::output("Appointment cron jobs completed.\n");
+    }
+
+
+    public function actionHello()
+    {
+        $this->stdout('Hello');
+    }
+
+    public function actionCheckPassedAppointments()
     {
 
         $appointments = Appointments::updatePassedAppointments();
-        
-        console::output($appointments);
 
+        console::output($appointments);
     }
 
-     public function actionSendAppointmentReminders()
-     {
-        // $date = date('Y-m-d H:1:s');
-     	$reminders = Appointments::getUpcomingAppointmentsForReminder();
-        $appointment = new Appointments();
+    public function actionSendAppointmentReminders()
+    {
+        $reminders = Appointments::getUpcomingAppointmentsForReminder();
 
-     	if(!empty($reminders)) {
-     		foreach($reminders as $reminder){
-     			 Console::output("Sending reminder for appointment");
-                $appointment->sendAppointmentsReminderEvent(
-                    $reminder->id,
-                    $reminder->email_address,
-                    $reminder->contact_name, 
-                    $reminder->appointment_date, 
-                    $reminder->start_time,
-                    $reminder->end_time, 
-                    $reminder->user_id);
+        if (!empty($reminders)) {
+            foreach ($reminders as $reminder) {
+                Console::output("Sending reminder for appointment");
+                $reminder->sendAppointmentsReminderEvent();
 
                 echo "[*] :: " . "Reminder sent to :" . $reminder->email_address . "\n";
-     		}
-     	} else {
-     		// $this->stdout('No reminders ');
+            }
+        } else {
+            // $this->stdout('No reminders ');
             echo "No reminders to send\n";
-     	}
-     }
+        }
+    }
+
+    public function actionAppointmentsPastOneHour()
+    {
+        $appointments = Appointments::getAppointmentsPastOneHour();
+
+        if (empty($appointments)) {
+            Console::output('No reminders found.');
+        } else {
+            foreach ($appointments as $appointment) {
+                // Console::output("Sending ");
+                Console::output("Meeting ID: {$appointment->id}, Ended at: {$appointment->appointment_date} {$appointment->end_time}");
+                $appointment->sendAppointmentCompletionReminder();
+            }
+        }
+    }
 }
