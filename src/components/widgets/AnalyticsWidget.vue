906d7f1278ff1aa9c2a23bcb4ed489fee75f0046
Itachi <bscs157j2022@students.tum.ac.ke>
2025-03-06 10:03:20 +0300

    centered icons and values

--- src/components/widgets/AnalyticsWidget.vue
+++ src/components/widgets/AnalyticsWidget.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="card">
     <div class="card-body">
-      <div class="d-flex align-items-center gap-3">
+      <div class="d-flex align-items-center justify-content-center gap-3">
         <div :class="`bg-${variant}-subtle p-2 rounded`">
           <!-- <icon-component type="dual-tone" :icon-name="icon" :size="32"></icon-component> -->
           <font-awesome-icon :icon="['fas', icon]" :style="{ color: Iconcolor }" class="anicon" />
         </div>
         <div>
-          <h4 class="counter">
+            <h4 class="counter text-center">
             <Vue3autocounter ref="counter" separator="" :duration="3" :startAmount="0" :endAmount="value" />
-          </h4>
+            </h4>
           <span class="text-dark">{{ description }}</span>
         </div>
       </div>
