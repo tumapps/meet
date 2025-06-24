3b2bee78858706d47a77111884e8244645d6f0bf
Itachi <bscs157j2022@students.tum.ac.ke>
2025-06-24 08:45:12 +0300

    url change and build files

---
+++ dist/assets/MeetingsApproval.39fd3cc2.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./spacesMiniTable.23d83178.js";import{o as r,g as e}from"./index.a8b7f511.js";import"./pdfjs.7adb8c57.js";const m={__name:"MeetingsApproval",setup(t){return(a,_)=>(r(),e(o))}};export{m as default};
